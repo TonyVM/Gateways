b999a4e40d8baa3615bb802fbe850ccb26395304
Antonio VM <antonio.valladaresmartinez@gmail.com>
2025-01-14 10:35:14 -0600

    some minor fixed related to env vars

--- database/mysql.js
+++ database/mysql.js
@@ -1,10 +1,12 @@
 import pmysql from 'promise-mysql';
+import {config} from 'dotenv';
+config();
 
 const connection = pmysql.createConnection({
-   host: "127.0.0.1",
-   database: "gateways_db",
-   user: "root",
-   password: "root123"
+   host: process.env.DB_HOST || "127.0.0.1",
+   database: process.env.DB || "gateways_db",
+   user: process.env.DB_USER || 'root',
+   password: process.env.DB_PASS || 'pass'
 });
 
 export const getConnectionMySQL = () => {
